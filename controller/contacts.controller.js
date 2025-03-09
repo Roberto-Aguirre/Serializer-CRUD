8ca79dec360360b56bcac50c49d7c2797b0ec9fe
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2025-03-09 12:46:15 -0600

    feat: Contact crud created

--- controller/contacts.controller.js
+++ controller/contacts.controller.js
@@ -5,12 +5,50 @@ export const getContacts = async (req, res) => {
     res.send(contacts);
 };
 
+export const getContact = async (req, res) => {
+    try {
+        let { id } = req.params;
+        let contact = await Contact.findByPk(id);
+        if (!contact)
+            return res.status(404).send({ message: 'Contact not found' });
+        res.send(contact);
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+};
+
 export const createContact = async (req, res) => {
     let contact = req.body;
     try {
         Contact.create(contact);
-        res.status(200).send({message:'Contact created'});
+        res.send({ message: 'Contact created' });
     } catch (error) {
-        res.status(500).send({message:error.message})
+        res.status(500).send({ message: error.message })
     }
-}
+};
+
+export const updateContact = async (req, res) => {
+    let contact = req.body;
+    let { id } = req.params;
+
+    try {
+        Contact.update(contact, { where: { id } });
+        res.send({ message: `User: ${id} Contact updated` });
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+};
+
+export const deleteContact = async (req, res) => {
+    let { id } = req.params;
+    try {
+        let contact = await Contact.findByPk(id);
+        if (!contact) {
+            return res.status(404).send({ message: 'Contact not found' });
+        }
+        Contact.update({ show: false }, { where: { id } });
+        res.send({ message: `User: ${id} contact deleted` });
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+};
--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createContact, getContacts } from "../controller/contacts.controller.js";
+import { createContact, deleteContact, getContact, getContacts, updateContact} from "../controller/contacts.controller.js";
 
 const router = Router()
 
 router.get('/contacts',getContacts);
-router.get('/contacts/:id');
+router.get('/contacts/:id',getContact);
 router.post('/contacts',createContact);
-router.put('/contacts/:id');
-router.delete('/contacts/:id');
+router.put('/contacts/:id',updateContact);
+router.delete('/contacts/:id',deleteContact);
 
 export default router;
